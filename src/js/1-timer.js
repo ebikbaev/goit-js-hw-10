62dc4b9068f53a64c887c802fdb9d49cde59269c
ebikbaev <bikbaevzno2020@gmail.com>
2024-06-26 11:04:22 +0300

    commit
    commit

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,10 +1,7 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-
-import imageUrl from '../img/alert-icon.svg';
 
 const timeInput = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('button[data-start]');
